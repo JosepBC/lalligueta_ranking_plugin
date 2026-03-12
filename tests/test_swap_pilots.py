5cffd29a568c9089a3a422c2a7aec3430e1a31af
JosepBC <bellocontacto@gmail.com>
2026-03-12 23:47:39 +0100

    Added test with missing pilots

--- tests/test_swap_pilots.py
+++ tests/test_swap_pilots.py
@@ -92,7 +92,22 @@ class TestSwapPilots(unittest.TestCase):
         self.assertEqual(result, expected_result)
 
     def test_minus_3_max_pilots_no_lap_per_group(self):
-        pass
+        # Heat rank is None if pilot hasn't done a singe pass on Start/Finish line
+        init_board = [[{'callsign': 'A', 'heat': 'H1', 'heat_rank': 1}, {'callsign': 'B', 'heat': 'H1', 'heat_rank': 2}, {'callsign': 'C', 'heat': 'H1', 'heat_rank': 3}, {'callsign': 'D', 'heat': 'H1', 'heat_rank': None}, {'callsign': 'E', 'heat': 'H1', 'heat_rank': None}],
+                      [{'callsign': 'F', 'heat': 'H2', 'heat_rank': 1}, {'callsign': 'G', 'heat': 'H2', 'heat_rank': 2}, {'callsign': 'H', 'heat': 'H2', 'heat_rank': 3}, {'callsign': 'I', 'heat': 'H2', 'heat_rank': None}, {'callsign': 'J', 'heat': 'H2', 'heat_rank': None}],
+                      [{'callsign': 'K', 'heat': 'H3', 'heat_rank': 1}, {'callsign': 'L', 'heat': 'H3', 'heat_rank': 2}, {'callsign': 'M', 'heat': 'H3', 'heat_rank': 3}, {'callsign': 'N', 'heat': 'H3', 'heat_rank': None}, {'callsign': 'O', 'heat': 'H3', 'heat_rank': None}],
+                      [{'callsign': 'P', 'heat': 'H4', 'heat_rank': 1}, {'callsign': 'Q', 'heat': 'H4', 'heat_rank': 2}, {'callsign': 'R', 'heat': 'H4', 'heat_rank': None}, {'callsign': 'S', 'heat': 'H4', 'heat_rank': None}],
+                      [{'callsign': 'U', 'heat': 'H5', 'heat_rank': 1}, {'callsign': 'V', 'heat': 'H5', 'heat_rank': 2}, {'callsign': 'W', 'heat': 'H5', 'heat_rank': None}]]
+
+        result = swap_on_grouped_board(init_board, logger)
+
+        expected_result = [[{'callsign': 'A', 'heat': 'H1', 'heat_rank': 1}, {'callsign': 'B', 'heat': 'H1', 'heat_rank': 2}, {'callsign': 'C', 'heat': 'H1', 'heat_rank': 3}, {'callsign': 'F', 'heat': 'H2', 'heat_rank': 1}, {'callsign': 'G', 'heat': 'H2', 'heat_rank': 2}],
+                            [{'callsign': 'D', 'heat': 'H1', 'heat_rank': None}, {'callsign': 'E', 'heat': 'H1', 'heat_rank': None}, {'callsign': 'H', 'heat': 'H2', 'heat_rank': 3}, {'callsign': 'K', 'heat': 'H3', 'heat_rank': 1}, {'callsign': 'L', 'heat': 'H3', 'heat_rank': 2}],
+                            [{'callsign': 'I', 'heat': 'H2', 'heat_rank': None}, {'callsign': 'J', 'heat': 'H2', 'heat_rank': None}, {'callsign': 'M', 'heat': 'H3', 'heat_rank': 3}, {'callsign': 'P', 'heat': 'H4', 'heat_rank': 1}, {'callsign': 'Q', 'heat': 'H4', 'heat_rank': 2}],
+                            [{'callsign': 'N', 'heat': 'H3', 'heat_rank': None}, {'callsign': 'O', 'heat': 'H3', 'heat_rank': None}, {'callsign': 'U', 'heat': 'H5', 'heat_rank': 1}, {'callsign': 'V', 'heat': 'H5', 'heat_rank': 2}],
+                            [{'callsign': 'R', 'heat': 'H4', 'heat_rank': None}, {'callsign': 'S', 'heat': 'H4', 'heat_rank': None}, {'callsign': 'W', 'heat': 'H5', 'heat_rank': None}]]
+
+        self.assertEqual(result, expected_result)
 
 if __name__ == "__main__":
     unittest.main()
